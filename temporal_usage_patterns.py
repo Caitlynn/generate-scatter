bcc52b198dc56c8b89db85bc4a2f0ba8be06669f
James Batt <place.net.au@gmail.com>
2017-06-03 14:50:51 +1000

    fixed

--- temporal_usage_patterns.py
+++ temporal_usage_patterns.py
@@ -11,16 +11,18 @@ def per_hour_stats():
             hour = date.strftime('%H')
             key = day + 'T' + hour
             d.setdefault(key, []).append(row)
-    output_csv_lines = []
+    output_csv_lines = [
+        'datetime,date,hour,total emojis,average emojis,total score,average scores,average body length'
+    ]
     for key, values in d.items():
         emoji_counts = list(map(lambda row: int(row[2]), values))
         total_emojis = sum(emoji_counts)
         average_emojis = total_emojis / len(emoji_counts)
-        scores = list(map(lambda row: int(row[3]), values))
+        body_lengths = list(map(lambda row: int(row[3]), values))
+        average_body_length = sum(body_lengths) / len(body_lengths)
+        scores = list(map(lambda row: int(row[4]), values))
         total_scores = sum(scores)
         average_scores = total_scores / len(scores)
-        sentiments = list(map(lambda row: float(row[4]), values))
-        average_sentiments = sum(sentiments) / len(sentiments)
         day, hour = key.split('T')
         csv_line = ','.join([
             key,
@@ -30,7 +32,7 @@ def per_hour_stats():
             str(average_emojis),
             str(total_scores),
             str(average_scores),
-            str(average_sentiments),
+            str(average_body_length)
         ])
         output_csv_lines.append(csv_line)
 
